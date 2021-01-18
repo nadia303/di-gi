ab211ae89f878e2fd4e523c93741ddcb9d551a6c
nadia303 <76038344+nadia303@users.noreply.github.com>
2021-01-19 00:54:48 +0200

    added media queries

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
-$(function(){
+$(document).ready(function() {
+
+    var owl = $("#owl-demo");
+
+    owl.owlCarousel({
+        items : 3, //10 items above 1000px browser width
+        itemsDesktop : [1000,3], //5 items between 1000px and 901px
+        itemsDesktopSmall : [900,3], // betweem 900px and 601px
+        itemsTablet: [600,3], //2 items between 600 and 0
+        itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
+    });
+
+    // Custom Navigation Events
+    $(".next").click(function(){
+        owl.trigger('owl.arrow-prev');
+    })
+    $(".prev").click(function(){
+        owl.trigger('owl.arrow-prev');
+    })
+    $(".play").click(function(){
+        owl.trigger('owl.play',1000); //owl.play event accept autoPlay speed as second parameter
+    })
+    $(".stop").click(function(){
+        owl.trigger('owl.stop');
+    })
 
 });
