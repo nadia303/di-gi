f50cdd3f9ccb3ceae073e81db22edfc239966157
nadia303 <76038344+nadia303@users.noreply.github.com>
2021-01-19 18:12:17 +0200

    added carusel

--- js/main.js
+++ js/main.js
@@ -1,27 +1,19 @@
 $(document).ready(function() {
 
-    var owl = $("#owl-demo");
+    $("#owl-demo").owlCarousel({
 
-    owl.owlCarousel({
-        items : 3, //10 items above 1000px browser width
-        itemsDesktop : [1000,3], //5 items between 1000px and 901px
-        itemsDesktopSmall : [900,3], // betweem 900px and 601px
-        itemsTablet: [600,3], //2 items between 600 and 0
-        itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
+        navigation : true, // Show next and prev buttons
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        singleItem:true
+
+
+        // items : 1,
+        // itemsDesktop : false,
+        // itemsDesktopSmall : false,
+        // itemsTablet: false,
+        // itemsMobile : false
+
     });
-
-    // Custom Navigation Events
-    $(".next").click(function(){
-        owl.trigger('owl.arrow-prev');
-    })
-    $(".prev").click(function(){
-        owl.trigger('owl.arrow-prev');
-    })
-    $(".play").click(function(){
-        owl.trigger('owl.play',1000); //owl.play event accept autoPlay speed as second parameter
-    })
-    $(".stop").click(function(){
-        owl.trigger('owl.stop');
-    })
 
 });
