6b3c9d7cf1c20c23cdad5101a03c294612f3db6d
nadia303 <76038344+nadia303@users.noreply.github.com>
2021-01-20 23:30:22 +0200

    corrected some mistakes

--- js/main.js
+++ js/main.js
@@ -1,31 +1,33 @@
-$(document).ready(function() {
+$(document).ready(function () {
     var owl = $("#intro_sliders");
 
     $("#intro_sliders").owlCarousel({
-        slideSpeed : 300,
-        paginationSpeed : 400,
-        singleItem:true
+        slideSpeed: 300,
+        paginationSpeed: 400,
+        singleItem: true,
+        autoPlay: 3000, //Set AutoPlay to 3 seconds
+    });
+    $("#intro-sliders").owlCarousel({
+
+        autoPlay: 3000, //Set AutoPlay to 3 seconds
+        items: 3,
+        itemsDesktop: [1199, 3],
+        itemsDesktopSmall: [979, 3]
+
     });
 
-
-
-
-    $(".next").click(function(){
+    $(".next").click(function () {
         owl.trigger('owl.next');
     })
-    $(".previous").click(function(){
+    $(".previous").click(function () {
         owl.trigger('owl.prev');
     })
 
 
-
-
-
-
     $("#testimonial_sliders").owlCarousel({
-        slideSpeed : 300,
-        paginationSpeed : 400,
-        singleItem:true
+        slideSpeed: 300,
+        paginationSpeed: 400,
+        singleItem: true
     });
 
 });
